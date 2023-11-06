baa75a4ca15edec39b793ebdb37d0d88ee926282
Melchiah Mauck <MelchiahMauck@gmail.com>
2023-11-06 09:16:06 -0800

    Set up initial project.

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const hello = () => {
+    return "Hello from @monode/mosa!";
+};
+exports.hello = hello;
