a072391eee5675e616773e270a5156a1e5bcd863
MechMel <melchiahmauck@gmail.com>
2024-03-20 10:33:51 -0700

    Publishing 0.0.10

--- src/Plugins/Solid.ts
+++ src/Plugins/Solid.ts
@@ -1,38 +1,39 @@
-import {
-  createEffect,
-  createMemo,
-  createRoot,
-  createSignal,
-  on,
-  onCleanup,
-} from "solid-js";
-import { MosaConfig } from "../Mosa.js";
+import { MosaConfig, initializeMosa } from "../Mosa.js";
 
 /** Use like:
  * ```ts
  * import { initializeMosa } from "mosa";
- * import { mosaConfigForSolid } from "mosa/solid";
+ * import { getMosaConfigForSolid } from "mosa/solid";
  *
- * module.exports = initializeMosa(mosaConfigForSolid);
+ * module.exports = initializeMosa(getMosaConfigForSolid({...}));
  * ```
  */
-export const mosaConfigForSolid = {
-  createRoot,
-  createSignal: (initValue) => {
-    const [get, set] = createSignal(initValue);
-    return { get, set };
-  },
-  createComputed: (compute) => {
-    return { get: createMemo(compute) };
-  },
-  createAutoEffect: createEffect,
-  createManualEffect: ({ on: onSignals, do: effect }) => {
-    createEffect(
-      on(
-        onSignals.map((signal) => () => signal.value),
-        effect,
-      ),
-    );
-  },
-  onDispose: onCleanup,
-} satisfies MosaConfig;
+export function initializeMosaForSolid(solidConfig: {
+  createRoot: <T>(func: () => T) => T;
+  createSignal<T>(initValue: T): [() => T, (newValue: T) => void];
+  createMemo<T>(get: () => T): () => T;
+  createEffect(effect: () => void): void;
+  on(deps: (() => any)[], effect: () => void): () => void;
+  onCleanup(func: () => void): void;
+}) {
+  return {
+    createRoot: solidConfig.createRoot,
+    createSignal: (initValue) => {
+      const [get, set] = solidConfig.createSignal(initValue);
+      return { get, set };
+    },
+    createComputed: (compute) => {
+      return { get: solidConfig.createMemo(compute) };
+    },
+    createAutoEffect: solidConfig.createEffect,
+    createManualEffect: ({ on: onSignals, do: effect }) => {
+      solidConfig.createEffect(
+        solidConfig.on(
+          onSignals.map((signal) => () => signal.value),
+          effect,
+        ),
+      );
+    },
+    onDispose: solidConfig.onCleanup,
+  } satisfies MosaConfig;
+}
